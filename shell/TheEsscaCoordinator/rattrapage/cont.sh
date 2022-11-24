94d7d2dc3204e8a81e58e103d45cb1e98378f1e9
emmachemineau <113505949+emmachemineau@users.noreply.github.com>
2022-11-24 15:50:43 +0100

    Add files via upload

---
+++ shell/TheEsscaCoordinator/rattrapage/cont.sh
@@ -0,0 +1,6 @@
+echo "Nombre d'étudiants qui vont au rattrapage:"
+wc -l < rattrapage.csv
+echo "Nombre d'étudiants qui ont validés:"
+wc -l < validation.csv
+echo "Nombre d'étudiants qui redoublent:"
+wc -l < redoublement.csv
